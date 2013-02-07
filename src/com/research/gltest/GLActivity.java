2fc14373f65cc043a4fb99c3c920370b0acb9ebe
yulu <yulu8798@gmail.com>
2013-02-07 21:01:59 +0800

    Add the files in

---
+++ src/com/research/gltest/GLActivity.java
@@ -0,0 +1,22 @@
+package com.research.gltest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class GLActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gl);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_gl, menu);
+        return true;
+    }
+    
+}
