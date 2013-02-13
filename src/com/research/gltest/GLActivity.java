0096bda1466c16d7e1e3853105e0d173339ad21b
yulu <yulu8798@gmail.com>
2013-02-13 16:39:32 +0800

    Revert "add all the files"
    This reverts commit 7a77713284dc69cbd56b9984085e9ed5e82c73fc.
    
    Conflicts:
    
    	bin/resources.ap_
    	res/raw/fragment_shader.glsl

--- src/com/research/gltest/GLActivity.java
+++ src/com/research/gltest/GLActivity.java
@@ -1,36 +1,22 @@
 package com.research.gltest;
 
-import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.view.Window;
+import android.view.Menu;
 
 public class GLActivity extends Activity {
-	GLSurfaceView mView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);   
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-       
-        mView = new GLSurfaceView(this);
-        mView.setEGLContextClientVersion(2);
-        mView.setRenderer(new GLLayer(this));
-        
-        setContentView(mView);
+        setContentView(R.layout.activity_gl);
     }
 
-    /** Called when the activity is first created. */
     @Override
-    public void onResume() {
-        super.onResume();
-        mView.onResume();
-       
-    }
-    protected void onPause() {
-    	super.onPause(); 
-    	mView.onPause();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_gl, menu);
+        return true;
     }
     
 }
